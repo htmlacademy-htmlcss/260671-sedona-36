d42c37c7b4a28922cdf00e1164f3b2d46e7d4881
sergentors705 <sergentors705@gmail.com>
2023-02-26 20:03:58 +0600

    finished PP fixes on filter section

--- script.js
+++ script.js
@@ -11,6 +11,49 @@ favoriteButtons.forEach(item => {
   });
 });
 
+
+// SLIDER
+let pinStart = document.querySelector('.slider__start-pin');
+let pinFinish = document.querySelector('.slider__finish-pin')
+let sliderContainer = document.querySelector('.slider-container');
+let sliderProcess = document.querySelector('.slider__process');
+let widthProcess = () => {
+    sliderProcess.style.left = pinStart.offsetLeft + "px";
+    sliderProcess.style.width = pinFinish.offsetLeft - pinStart.offsetLeft + "px";
+}
+widthProcess();
+function mouseDown (evt){
+    evt.preventDefault();
+    let startCords = evt.clientX
+    function mouseMove(evtMove) {
+        let shift = startCords - evtMove.clientX
+        startCords = evtMove.clientX
+        let cordX = evt.target.offsetLeft - shift;
+        if(evt.target.classList[0] === "slider__start-pin" && cordX < 0){
+            cordX = 0;
+        } else if (evt.target.classList[0] === "slider__start-pin" && cordX > pinFinish.offsetLeft - 20){
+            cordX = pinFinish.offsetLeft - 20;
+        }
+        if(evt.target.classList[0] === "slider__finish-pin" && cordX > 300 -20){
+            cordX = 300 -20;
+        } else if (evt.target.classList[0] === "slider__finish-pin" && cordX < pinStart.offsetLeft + 20){
+            cordX = pinStart.offsetLeft + 20;
+        }
+        evt.target.style.left = cordX + 'px';
+        widthProcess();
+
+    }
+    function mouseUp () {
+        sliderContainer.removeEventListener('mousemove', mouseMove);
+        document.removeEventListener('mouseup', mouseUp);
+    }
+    sliderContainer.addEventListener('mousemove', mouseMove);
+    document.addEventListener('mouseup', mouseUp);
+}
+pinStart.addEventListener('mousedown', mouseDown);
+pinFinish.addEventListener('mousedown', mouseDown);
+
+
 // MODAL WINDOW
 const modalOpenButton = document.querySelectorAll(".modal-open-js");
 const modal = document.querySelector(".modal-container");
@@ -41,44 +84,3 @@ const tooltipShow = () => {
 }
 
 tooltipButton.addEventListener("click", tooltipShow);
-
-// SLIDER
-let pinStart = document.querySelector('.slider__start-pin');
-let pinFinish = document.querySelector('.slider__finish-pin')
-let skiderContainer = document.querySelector('.slider-container');
-let sliderProcess = document.querySelector('.slider__process');
-let widthProcess = () => {
-    sliderProcess.style.left = pinStart.offsetLeft + "px";
-    sliderProcess.style.width = pinFinish.offsetLeft - pinStart.offsetLeft + "px";
-}
-widthProcess();
-function mouseDown (evt){
-    evt.preventDefault();
-    let startCords = evt.clientX
-    function mouseMove(evtMove) {
-        let shift = startCords - evtMove.clientX
-        startCords = evtMove.clientX
-        let cordX = evt.target.offsetLeft - shift;
-        if(evt.target.classList[0] === "slider__start-pin" && cordX < 0){
-            cordX = 0;
-        } else if (evt.target.classList[0] === "slider__start-pin" && cordX > pinFinish.offsetLeft - 20){
-            cordX = pinFinish.offsetLeft - 20;
-        }
-        if(evt.target.classList[0] === "slider__finish-pin" && cordX > 300 -20){
-            cordX = 300 -20;
-        } else if (evt.target.classList[0] === "slider__finish-pin" && cordX < pinStart.offsetLeft + 20){
-            cordX = pinStart.offsetLeft + 20;
-        }
-        evt.target.style.left = cordX + 'px';
-        widthProcess();
-
-    }
-    function mouseUp () {
-        skiderContainer.removeEventListener('mousemove', mouseMove);
-        document.removeEventListener('mouseup', mouseUp);
-    }
-    skiderContainer.addEventListener('mousemove', mouseMove);
-    document.addEventListener('mouseup', mouseUp);
-}
-pinStart.addEventListener('mousedown', mouseDown);
-pinFinish.addEventListener('mousedown', mouseDown);
